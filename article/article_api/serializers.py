70395600e8b87ea5465205c2550164672d6028f1
nurxon <nafasovnurxon18@gmail.com>
2024-03-03 22:54:57 +0500

    modified

--- article/article_api/serializers.py
+++ article/article_api/serializers.py
@@ -1,10 +1,22 @@
 
 from rest_framework import serializers
 
-from article.models import Article
+from article.models import Article, Tags, Category
 
 
 class ArticleSerializer(serializers.ModelSerializer):
     class Meta:
         model = Article
         fields = ['id', 'author', 'title', 'tags', 'category', 'image', 'modified_date', 'created_date']
+
+
+class TagSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Tags
+        fields = ['id', 'title']
+
+
+class CategorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Category
+        fields = ['id', 'title']
--- article/article_api/view.py
+++ article/article_api/view.py
@@ -2,15 +2,24 @@ from django.shortcuts import get_object_or_404
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
 
-from .serializers import ArticleSerializer
-from article.models import Article
+
+from .serializers import ArticleSerializer, TagSerializer, CategorySerializer
+from article.models import Article, Category, Tags
 
 
 @api_view(['GET'])
-def article_list_api_view(request,*args,**kwargs):
+def article_list_api_view(request, *args, **kwargs):
     articles = Article.objects.all().order_by('-id')
-    serializer = ArticleSerializer(articles, many=True)
-    return Response(serializer.data)
+    tags = Tags.objects.all()
+    categories = Category.objects.all()
+    tags_serializer = TagSerializer(tags, many=True)
+    categories_serializer = CategorySerializer(categories, many=True)
+    article_serializer = ArticleSerializer(articles, many=True)
+    context = {
+        'categories_serializer': categories_serializer,
+        'article_serializer': article_serializer,
+    }
+    return Response(article_serializer.data)
 
 
 @api_view(['GET'])
